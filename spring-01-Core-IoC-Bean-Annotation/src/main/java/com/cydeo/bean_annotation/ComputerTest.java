59ddaa4dda6b62727654bcae8a5a3d2d5b75bcce
Asmik <jasmin_ayr@mail.ru>
2024-03-19 19:20:24 +0100

    Updated ComputerTest Class with @Primary example

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,13 +1,12 @@
 package com.cydeo.bean_annotation;
 
-import com.cydeo.bean_annotation.casefactory.Case;
+
 import com.cydeo.bean_annotation.casefactory.DellCase;
 import com.cydeo.bean_annotation.config.ComputerConfig;
 import com.cydeo.bean_annotation.config.RandomConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
-import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
