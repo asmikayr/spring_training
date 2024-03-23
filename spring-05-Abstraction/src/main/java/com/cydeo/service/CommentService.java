dd5b1b376268eb25fadf1aed4c254ecfe42c129a
Asmik <jasmin_ayr@mail.ru>
2024-03-23 15:34:43 +0100

    Added PushComment implementation too

--- spring-05-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,11 +3,19 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class CommentService {
-private CommentRepository commentRepository;
-private CommentNotificationProxy commentNotificationProxy;
+private final CommentRepository commentRepository;
+private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository,@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);//save in the DB
         commentNotificationProxy.sendComment(comment);//send email
