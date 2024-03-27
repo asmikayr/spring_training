ef15a8e952723c0f0d81c02347f77914237feeee
Asmik <jasmin_ayr@mail.ru>
2024-03-27 23:48:42 +0100

    Add MVC Controller and Model example

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication1.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CydeoApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CydeoApplication1.class, args);
+    }
+
+}
