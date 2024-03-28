55df95cf60bc0de8574edff588cf954f040c4580
Asmik <jasmin_ayr@mail.ru>
2024-03-28 09:05:25 +0100

    CarController created

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,32 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarController {
+
+    //localhost:2020/info?make=Honda
+@RequestMapping("/info")
+    public String carInfo(@RequestParam String make, Model model){
+    model.addAttribute("make", make);
+        return "car/car-info";
+    }
+
+    //localhost:2020/info2(KIA)
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make", required = false, defaultValue = "KIA") String make, Model model){
+        model.addAttribute("make", make);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info3")
+    public String carInfo3(@RequestParam String make, @RequestParam int year, Model model){//localhost:2020/info3?make=Honda&year=2015
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+
+        return "car/car-info";
+    }
+}
