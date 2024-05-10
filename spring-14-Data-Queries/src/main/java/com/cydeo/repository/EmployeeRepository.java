80937be87c52de62aef0edc0aef559defe5655ee
Asmik <jasmin_ayr@mail.ru>
2024-05-10 21:20:00 +0200

    EmployeeRepository created

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    //Display all employees with email address ""
+    List<Employee> findByEmail(String email);
+
+    //Display all employees with firstname "" and lastname "",
+    //also show all employees with an email address ""
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstname, String lastname, String email);
+
+    //Display all employees that first name is not ""
+    List<Employee> findByFirstNameIsNot(String firstname);
+
+
+}
