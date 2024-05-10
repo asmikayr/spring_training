166e49bedcb4f028efdd01f6f5b488a6064c7b5e
Asmik <jasmin_ayr@mail.ru>
2024-05-10 23:19:41 +0200

    EmployeeRepository done

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
@@ -16,6 +17,28 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //Display all employees that first name is not ""
     List<Employee> findByFirstNameIsNot(String firstname);
+
+    //Display all employees where last name starts with ""
+    List<Employee> findByLastNameStartsWith(String pattern);
+
+    //Display all employees with salaries higher than ""
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //Display all employees with salaries higher than ""
+    List<Employee> findBySalaryLessThan(Integer salary);
+
+    //Display all employees that has been hired between "" and ""
+    List<Employee> findByHireDateBetween(LocalDate date1, LocalDate date2);
+
+    //Display all employees where salaries greater and equal to "" i order-salary
+    List<Employee> findBySalaryGreaterThanEqualOrderBySalary(Integer salary);
+
+    //Display top unique 3 employees that is making less than ""
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    //Display all employees that do not have email address
+    List<Employee> findByEmailIsNull();
+
 
 
 }
