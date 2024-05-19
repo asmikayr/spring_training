f762bc8cbddbc612c23ef6870fe1b0cebe3c67a1
Asmik <jasmin_ayr@mail.ru>
2024-05-19 22:32:44 +0200

    CinemaRepository modified

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -1,10 +1,8 @@
 package com.cydeo.repository;
 
-import com.cydeo.entity.Account;
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
