53d27a7b443f645e47a756f00adc7433809c3058
Asmik <jasmin_ayr@mail.ru>
2024-06-17 22:59:04 +0200

    CourseController getAllCourses, getCoursebyId and Categy end points added

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -1,18 +1,37 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
-import org.springframework.stereotype.Controller;
+import com.cydeo.service.CourseService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/courses")
 public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
     @GetMapping
     public List<CourseDTO> getAllCourses(){
+        return courseService.getCourses();
+    }
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") long courseId){
+        return courseService.getCourseById(courseId);
+    }
 
+    @GetMapping("category/{categoryName}")
+    public List<CourseDTO> getCourseById(@PathVariable("categoryName") String categoryName){
+        return courseService.getCoursesByCategory(categoryName);
     }
 
 
