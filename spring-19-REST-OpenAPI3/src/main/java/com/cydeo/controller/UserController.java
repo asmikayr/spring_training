fce78af4b14ac4aedb6b672ff316a75de0dc1e85
Asmik <jasmin_ayr@mail.ru>
2024-06-23 19:21:49 +0200

    UserController updated

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
