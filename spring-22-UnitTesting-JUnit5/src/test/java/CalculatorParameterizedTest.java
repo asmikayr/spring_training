d04bd7e63136d727d0b132fa81d1737f57a665db
Asmik <jasmin_ayr@mail.ru>
2024-07-16 23:55:01 +0200

    Spring 22 - UnitTesting - CalculatorParameterizedTest - @ValueSource examples added

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorParameterizedTest {
@@ -17,6 +20,17 @@ public class CalculatorParameterizedTest {
     void testCase1(int num){
 
         Assertions.assertEquals(0, num%3);
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "JS", "TS"})
+    //@EmptySource
+   // @NullSource
+    @NullAndEmptySource
+    void testCase3(String arg){
+
+        Assertions.assertFalse(arg.isEmpty());
 
     }
 
