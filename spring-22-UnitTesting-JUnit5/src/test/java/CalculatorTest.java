fd2ff6ffeeeed0b6b3d5743c0f76ef51d64e3361
Asmik <jasmin_ayr@mail.ru>
2024-07-16 23:42:16 +0200

    Spring 22 - UnitTesting - CalculatorTest - BeforeAll After all added

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,12 +1,43 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.AccessDeniedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @BeforeAll
+    static void setUpAll(){
+        System.out.println("BeforeAll is executed");
+    }
+    @AfterAll
+    static void tearDownAll(){
+        System.out.println("AfterAll is executed");
+    }
+    @BeforeEach
+    void setUpEach(){
+        System.out.println("BeforeEach is executed.");
+    }
+
+    @AfterEach
+    void tearDownEach(){
+        System.out.println("AfterEach is executed");
+    }
+
+
 
     @Test
     void add() {
         System.out.println("Add method");
+        int actual = Calculator.add(2,3);
+        assertEquals(5, actual);
+    }
+
+    @Test
+    void add2() {
+        System.out.println("Add method2");
+        //assertThrows(IllegalArgumentException.class, ()-> Calculator.add2(3,2));
+        //assertThrows(AccessDeniedException.class, () -> Calculator.add2(3,2));
+        assertThrows(IllegalArgumentException.class, ()->Calculator.add2(2,3));
     }
 
     @Test
@@ -17,6 +48,8 @@ class CalculatorTest {
     @Test
     void testCase2(){
         System.out.println("Test Case 2");
+        assertEquals("add", Calculator.operator);
+        assertTrue(Calculator.operator.equals("add"));
     }
 
     @Test
@@ -30,6 +63,12 @@ class CalculatorTest {
     @Test
     void testCase5(){
         System.out.println("Test Case 5");
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertSame(c1, c2);
+        assertNotSame(c1, c3);
     }
 
 }
